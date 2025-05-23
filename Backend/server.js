c6d7f5a1145e306e77a5174208e8c5b94c951097
Aditya Burman <burman.aditya22@gmail.com>
2025-05-23 18:02:08 +0530

    initial commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,18 @@
-require('dotenv').config()
-const app = require('./src/app')
+const express = require('express');
+const path = require('path');
+require('dotenv').config();
+const app = require('./src/app');
 
+const port = process.env.PORT || 3000;
 
+if (process.env.NODE_ENV === 'production') {
+    const __dirname = path.resolve();
+    app.use(express.static(path.join(__dirname, 'Frontend', 'dist')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'Frontend', 'dist', 'index.html'));
+    });
+}
 
-app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000')
-})
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
